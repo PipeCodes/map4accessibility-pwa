a9fe7d4b8ff9560375cc5748eb399598d97577ea
Alexandre Pereira <alexandre.pereira@pipecodes.com>
2022-10-19 16:58:06 +0100

    Validation fix

--- src/screens/RegisterScreen/RegisterScreen.jsx
+++ src/screens/RegisterScreen/RegisterScreen.jsx
@@ -58,25 +58,25 @@ const RegisterScreen = (props) => {
     const errors = {};
     const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
     const regexPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    const regexDate =
-      /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+    const regexDate = /^[0-9]{1,4}[-,/][0-9]{1,2}[-,/][0-9]{1,4}$/;
 
     if (!values.firstName) {
       errors.firstName = t('required_firstName');
-    } else if (values.firstName !== 'string') {
+    } else if (typeof values.firstName !== 'string') {
       errors.firstName = t('string_firstName');
     }
 
     if (!values.surname) {
       errors.surname = t('required_surname');
-    } else if (values.surname !== 'string') {
-      errors.firstName = t('string_firstName');
+    } else if (typeof values.surname !== 'string') {
+      errors.surname = t('string_firstName');
     }
 
     if (!values.birthDate) {
       errors.birthDate = t('required_birthDate');
     } else if (!regexDate.test(values.birthDate)) {
-      errors.email = t('invalid_birthDate');
+      console.log(values.birthDate);
+      errors.birthDate = t('invalid_birthDate');
     }
 
     if (!values.email) {
