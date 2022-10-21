3e078b8c1153de7ef107b94fb8214ac96241732f
José Cigarro <jose.cigarro@pipecodes.com>
2022-10-21 07:32:38 +0100

    login

--- src/screens/LoginScreen/LoginScreen.jsx
+++ src/screens/LoginScreen/LoginScreen.jsx
@@ -1,99 +1,153 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
 import { withRouter } from 'react-router-dom';
-import CustomInput from '../../components/CustomInput/CustomInput';
-import { colors } from '../../constants/colors';
-import CustomButton from '../../components/CustomButton/CustomButton';
-import TopBar from '../../components/TopBar/TopBar';
-import AccessibilityIcon from '../../assets/icons/accessibility.svg';
-import {
-  Page,
-  LogoImage,
-  RecoverPassword,
-  Subtitle,
-} from './LoginScreen.styles';
-import Logo from '../../assets/images/old_delete/logo.svg';
+import { useTranslation } from 'react-i18next';
 import { login } from '../../store/actions/auth';
+import { Page, Container, Box, TextSecondary } from './LoginScreen.styles';
+import CustomButton from '../../components/CustomButton/CustomButton';
+import { colors } from '../../constants/colors';
+import OpenAccountLogin from '../../assets/icons/open-account-login.svg';
+import TopBar from '../../components/TopBar/TopBar';
+import SignUpInfo from './SignUpInfo';
+
+const PageDisplay = (formData, setFormData, page, formErrors) => {
+  if (page === 0) {
+    return (
+      <SignUpInfo
+        formData={formData}
+        setFormData={setFormData}
+        formErrors={formErrors}
+      />
+    );
+  }
+};
 
 const LoginScreen = (props) => {
-  const { routes, history } = props;
+  const initialValues = {
+    email: '',
+    password: '',
+  };
 
+  const { history, routes } = props;
   const { t } = useTranslation();
   const dispatch = useDispatch();
+  const fontSize = useSelector((state) => state.accessibility.fontSize);
+  const [page, setPage] = useState(0);
+  const [formData, setFormData] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
 
-  const loading = useSelector((state) => state.auth.loading);
-  const user = useSelector((state) => state.auth.user);
+  function changePage(value) {
+    setPage((currPage) => currPage + value);
+  }
 
-  const [emailOrUsername, setEmailOrUsername] = useState('');
-  const [password, setPassword] = useState('');
+  // Validates the fields
+  const validate = (values) => {
+    const errors = {};
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    const regexPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    if (!values.email) {
+      errors.email = t('required_email');
+    } else if (!regexEmail.test(values.email)) {
+      errors.email = t('invalid_email');
+    }
+
+    if (!values.password) {
+      errors.password = t('required_password');
+    } else if (values.password !== values.confirmPassword) {
+      errors.password = t('passwords_match');
+    } else if (!regexPassword.test(values.password)) {
+      errors.password = t('password_rules');
+    }
+
+    return errors;
+  };
+
+  // ClickHandlers
+  const registerClickHandler = useCallback(() => {
+    dispatch(login(formData.email, formData.password)).catch((error) => {
+      alert(error);
+    });
+  }, [dispatch, formData]);
 
   const openAccessibility = useCallback(() => {
     history.push(routes.ACCESSIBILITY.path);
   }, [history, routes]);
 
-  const loginClickHandler = () => {
-    dispatch(login(emailOrUsername, password, t)).catch((error) => {
-      alert(error);
-    });
+  const backClickHandler = (page) => {
+    if (page === 0) {
+      history.goBack();
+    }
+    changePage(-1);
   };
 
-  useEffect(() => {
-    if (user) {
-      alert('TODO');
+  const nextClickHandler = (page, formErrors) => {
+    if (page === 0) {
+      setFormErrors(validate(formData));
+      if (Object.keys(validate(formData)).length === 0) {
+        changePage(1);
+        return;
+      }
+      return;
     }
-  }, [user]);
+    if (Object.keys(formErrors).length === 0) {
+      registerClickHandler();
+    } else {
+      setPage(0);
+    }
+  };
 
   return (
     <Page>
-      <TopBar hasAccessibilityButton={openAccessibility} />
-      <LogoImage className="logo_img" alt="logo" src={Logo} />
-      <Subtitle>{t('login_subtitle')}</Subtitle>
-      <CustomInput
-        style={{
-          marginTop: 20,
-        }}
-        placeholder={t('username_email')}
-        type="email"
-        onChange={(e) => setEmailOrUsername(e.target.value)}
-        value={emailOrUsername}
+      <TopBar
+        backTarget={() => backClickHandler(page)}
+        aligned
+        hasBackButton
+        hasLogo
+        hasAccessibilityButton={openAccessibility}
       />
-      <CustomInput
-        style={{
-          marginTop: 13,
-        }}
-        placeholder={t('password')}
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <RecoverPassword
-        onClick={() => history.push(routes.RECOVER_PASSWORD.path)}
-      >
-        {t('recover_password')}
-      </RecoverPassword>
+      <Container>
+        {PageDisplay(formData, setFormData, page, formErrors)}
+        <CustomButton
+          style={{
+            marginTop: 30,
+            marginBottom: 20,
+            width: '100%',
+            borderRadius: '25px',
+          }}
+          backgroundColor={colors.orange}
+          text={t('login')}
+          icon={OpenAccountLogin}
+          onClick={() => nextClickHandler(page, formErrors)}
+        />
 
-      <CustomButton
-        style={{
-          marginTop: 20,
-        }}
-        backgroundColor={colors.green}
-        text={t('login')}
-        onClick={loginClickHandler}
-        disabled={loading}
-      />
-
-      <Subtitle>{t('dont_have_an_account_yet')}</Subtitle>
-
-      <CustomButton
-        style={{
-          marginTop: 10,
-        }}
-        backgroundColor={colors.orange}
-        text={t('register')}
-        onClick={() => history.push(routes.REGISTER_OPTIONS.path)}
-        disabled={loading}
-      />
+        {page === 0 && (
+          <Box>
+            <CustomButton
+              style={{
+                width: 'auto',
+                color: colors.primaryColor,
+                fontSize: { fontSize },
+                boxShadow: 'none',
+              }}
+              backgroundColor={colors.transparent}
+              text={t('create_account')}
+              onClick={() => history.push(routes.REGISTER.path)}
+            />
+            <CustomButton
+              style={{
+                width: 'auto',
+                color: colors.primaryColor,
+                fontSize: { fontSize },
+                boxShadow: 'none',
+              }}
+              backgroundColor={colors.transparent}
+              text={t('recover_password')}
+              onClick={() => history.push(routes.LOGIN.path)}
+            />
+          </Box>
+        )}
+      </Container>
     </Page>
   );
 };
---
+++ src/screens/LoginScreen/SignUpInfo.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import CustomInput from '../../components/CustomInput/CustomInput';
+import { Text, InputLabel, Error } from './LoginScreen.styles';
+
+const SignUpInfo = (props) => {
+  const { formData, setFormData, formErrors } = props;
+  const { t } = useTranslation();
+  const fontSize = useSelector((state) => state.accessibility.fontSize);
+
+  return (
+    <div className="fullDiv">
+      <Text fontSize={fontSize}>{t('login')}</Text>
+
+      <InputLabel fontSize={fontSize}>
+        {t('email')}
+        <span>*</span>
+      </InputLabel>
+      <CustomInput
+        style={{}}
+        placeholder={t('email_placeholder')}
+        type="email"
+        value={formData.email}
+        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+      />
+      {formErrors.email && (
+        <Error fontSize={fontSize}>{formErrors.email}</Error>
+      )}
+      <InputLabel fontSize={fontSize}>
+        {t('password')}
+        <span>*</span>
+      </InputLabel>
+      <CustomInput
+        style={{}}
+        placeholder={t('password_placeholder')}
+        type="password"
+        value={formData.password}
+        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+      />
+      {formErrors.password && (
+        <Error fontSize={fontSize}>{formErrors.password}</Error>
+      )}
+    </div>
+  );
+};
+
+export default SignUpInfo;
