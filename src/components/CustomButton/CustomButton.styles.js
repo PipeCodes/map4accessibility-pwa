ca2d5522bc1c262f8acb4fc937dea11d6f0683c7
Alexandre Pereira <alexandre.pereira@pipecodes.com>
2022-10-28 10:49:48 +0100

    Fields Validation and screen interactions

--- src/components/CustomButton/CustomButton.styles.js
+++ src/components/CustomButton/CustomButton.styles.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import { updateFontSize } from '../../helpers/utils';
-import { colors } from '../../constants/colors';
 
 const ButtonStyle = styled.button`
   background-color: ${(props) => props.backgroundColor};
@@ -9,8 +8,9 @@ const ButtonStyle = styled.button`
   color: ${(props) => (props.color ? props.color : '#ffffff')};
   border: ${(props) => (props.border ? props.border : 0)};
   border-radius: ${(props) => (props.borderRadius ? props.borderRadius : 0)};
+  margin: ${(props) => (props.margin ? props.margin : 'unset')};
   margin-top: ${(props) => (props.marginTop ? props.marginTop : 0)};
-  font-size: ${(props) => updateFontSize(20, props.fontSize)};
+  font-size: ${(props) => updateFontSize(16, props.fontSize)};
   width: ${(props) => (props.width ? props.width : '189px')};
   height: ${(props) => (props.height ? props.height : 'auto')};
   min-height: ${(props) => (props.minHeight ? props.minHeight : '42px')};
--- src/screens/ProfileScreen/ProfileScreen.styles.js
+++ src/screens/ProfileScreen/ProfileScreen.styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import BackgroundImage from '../../assets/images/old_delete/bg_blue.svg';
 import { colors } from '../../constants/colors';
+import { updateFontSize, updateValue } from '../../helpers/utils';
 
 export const Page = styled.div`
   display: flex;
@@ -8,6 +8,8 @@ export const Page = styled.div`
   align-items: center;
   height: 100%;
   z-index: 54;
+  background-color: ${(props) =>
+    updateValue(colors.transparent, props.backgroundColor)};
 `;
 
 export const Container = styled.div`
@@ -17,6 +19,7 @@ export const Container = styled.div`
   align-items: center;
   z-index: 54;
   width: 100%;
+  padding-bottom: 92px;
 
   .fullDiv {
     height: 100%;
@@ -26,4 +29,110 @@ export const Container = styled.div`
     flex-direction: column;
     align-items: center;
   }
+`;
+
+export const TopWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 20px;
+`;
+
+export const FormWrapper = styled.div`
+  width: 100%;
+
+  input {
+    border: none;
+    border-radius: 4px;
+    box-shadow: 0px 2px 2px #0000000a;
+    font-size: ${(props) => updateFontSize(14, props.fontSize)};
+    font-family: ${(props) => updateValue('EasyReadingPro', props.font)};
+    padding: 5px;
+    padding-left: 10px;
+    color: ${colors.grey};
+    margin-bottom: 10px;
+    margin-top: 5px;
+  }
+`;
+
+export const BottomWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Link = styled.a`
+  color: ${colors.grey};
+  font-size: ${(props) => updateFontSize(14, props.fontSize)};
+  font-family: ${(props) => updateValue('NotoSans-Regular', props.font)};
+  margin-bottom: 10px;
+`;
+
+export const RankingButton = styled.button`
+  border-radius: 50%;
+  background: ${colors.primaryColor};
+  box-shadow: 0px 2px 2px #00000029;
+  border: none;
+  color: white;
+  margin: 0;
+  width: 32px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StackContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Avatar = styled.img`
+  width: 80px;
+  height: 80px;
+  object-fit: cover;
+  border-radius: 50%;
+  border: 3px solid ${colors.primaryColor};
+`;
+
+export const Name = styled.span`
+  font-size: ${(props) => updateFontSize(20, props.fontSize)};
+  font-family: ${(props) => updateValue('EasyReadingPro', props.font)};
+  color: ${colors.primaryTextColor};
+  margin-top: 5px;
+`;
+
+export const EditButton = styled.button`
+  border-radius: 50%;
+  background: transparent;
+  box-shadow: 0px 2px 2px #00000029;
+  border: 2px solid ${colors.primaryColor};
+  color: white;
+  margin: 0;
+  width: 32px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &.active {
+    background: ${colors.primaryColor};
+  }
+`;
+
+export const InputLabel = styled.span`
+  font-size: ${(props) => updateFontSize(16, props.fontSize)};
+  font-family: ${(props) => updateValue('EasyReadingPro', props.font)};
+  color: ${colors.primaryTextColor};
+  place-self: start;
+  margin-top: 10px;
+`;
+
+export const Error = styled.div`
+  font-size: ${(props) => updateFontSize(10, props.fontSize)};
+  place-self: start;
+  color: orange;
+  margin-left: 10px;
 `;
