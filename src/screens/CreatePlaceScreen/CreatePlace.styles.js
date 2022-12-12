5fcaed020f4ea95816442e2fede534afa5138ea6
Alexandre Pereira <alexandre.pereira@pipecodes.com>
2022-12-12 16:58:58 +0000

    Initial Structure Create Place

---
+++ src/screens/CreatePlaceScreen/CreatePlace.styles.js
@@ -0,0 +1,133 @@
+import styled from 'styled-components';
+import { colors } from '../../constants/colors';
+import { updateValue, updateFontSize } from '../../helpers/utils';
+
+export const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: '100%';
+  z-index: 54;
+  background-color: ${(props) =>
+    updateValue(colors.transparent, props.backgroundColor)};
+
+  .slick-slider {
+    width: 100%;
+
+    .slick-dots {
+      bottom: 0;
+      li {
+        button:before {
+          opacity: 0.5;
+          color: white !important;
+          font-size: 8px !important;
+        }
+      }
+
+      .slick-active button:before {
+        opacity: 1;
+      }
+    }
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  padding: 0px 15px;
+  flex-direction: column;
+  align-items: center;
+  z-index: 54;
+  width: 100%;
+  padding-bottom: 92px;
+  margin-top: 30px;
+
+  .fullDiv {
+    height: 100%;
+    width: 100%;
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const TextWrapper = styled.div`
+  min-width: 200px;
+  margin-left: 15px;
+  align-self: start;
+`;
+
+export const Name = styled.div`
+  font-size: ${(props) => updateFontSize(20, props.fontSize)};
+  font-family: ${(props) => updateValue('EasyReadingPro', props.font)};
+  color: ${colors.primaryTextColor};
+`;
+
+export const City = styled.div`
+  font-size: ${(props) => updateFontSize(14, props.fontSize)};
+  font-family: ${(props) => updateValue('EasyReadingPro', props.font)};
+  color: ${colors.grey};
+`;
+
+export const Text = styled.span`
+  font-size: ${(props) => updateFontSize(20, props.fontSize)};
+  font-family: ${(props) => updateValue('EasyReadingPro', props.font)};
+  color: ${colors.primaryTextColor};
+  margin-top: 15px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  width: 100%;
+  text-align: center;
+  border-top: 1px solid ${colors.lightGrey};
+`;
+
+export const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: start;
+  width: 100%;
+`;
+
+export const Label = styled.span`
+  font-size: ${(props) => updateFontSize(16, props.fontSize)};
+  font-family: ${(props) => updateValue('EasyReadingPro', props.font)};
+  color: ${colors.primaryTextColor};
+  margin-top: 30px;
+  margin-bottom: 10px;
+`;
+
+export const Title = styled.span`
+  font-size: ${(props) => updateFontSize(16, props.fontSize)};
+  font-family: ${(props) => updateValue('EasyReadingPro', props.font)};
+  color: ${colors.grey};
+  margin: 10px 0 15px 0;
+`;
+
+export const ButtonContainer = styled.div`
+  background-color: ${colors.primaryColor};
+  padding: 5px;
+  box-shadow: 0px 2px 2px #0000000a;
+  border-radius: 3px;
+  margin-top: 20px;
+  width: 100%;
+`;
+
+export const MediaLabel = styled.div`
+  font-size: ${(props) => updateFontSize(12, props.fontSize)};
+  font-family: ${(props) => updateValue('EasyReadingPro', props.font)};
+  margin-top: 10px;
+  margin-bottom: 50px;
+  color: grey;
+  span {
+    color: ${colors.primaryColor};
+  }
+`;
+
+export const Error = styled.div`
+  font-size: ${(props) => updateFontSize(10, props.fontSize)};
+  font-family: ${(props) => updateValue('EasyReadingPro', props.font)};
+  place-self: start;
+  color: orange;
+  margin-left: 10px;
+  margin-bottom: 10px;
+`;
