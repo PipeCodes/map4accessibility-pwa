f9a8c0d528db04d0ea64c0d695ecd658f886ce7a
Alexandre Pereira <alexandre.pereira@pipecodes.com>
2023-01-18 10:58:42 +0000

    Changing details

---
+++ src/components/PlacePopUp/PlacePopUp.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import ArrowLeft from '../../assets/icons/arrow-left.svg';
+
+import {
+  PopUp,
+  DarkOverlayContainer,
+  Icon,
+  Text,
+  TextSecondary,
+  Container,
+} from './PlacePopUp.styles';
+import CustomButton from '../CustomButton/CustomButton';
+
+import { colors } from '../../constants/colors';
+
+const PlacePopUpComponent = (props) => {
+  const { title, text, buttonText, action, buttonIcon, icon } = props;
+  const fontSize = useSelector((state) => state.accessibility.fontSize);
+  const backgroundColor = useSelector(
+    (state) => state.accessibility.backgroundColor,
+  );
+  return (
+    <Container>
+      <DarkOverlayContainer />
+      <PopUp backgroundColor={backgroundColor}>
+        <Icon>
+          <img src={icon} alt="back" />
+        </Icon>
+        <Text fontSize={fontSize}>{title}</Text>
+        <TextSecondary fontSize={fontSize}>{text} </TextSecondary>
+        <CustomButton
+          style={{
+            marginTop: 30,
+            marginBottom: 20,
+            width: '100%',
+            borderRadius: '25px',
+            color: colors.grey,
+            border: '1px solid grey',
+          }}
+          backgroundColor={colors.transparent}
+          text={buttonText}
+          onClick={action}
+          buttonicon={buttonIcon}
+          icon={ArrowLeft}
+        />
+      </PopUp>
+    </Container>
+  );
+};
+
+export default PlacePopUpComponent;
---
+++ src/components/PlacePopUp/PlacePopUp.styles.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import { updateFontSize, updateValue } from '../../helpers/utils';
+import { colors } from '../../constants/colors';
+
+export const Container = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+`;
+
+export const PopUp = styled.div`
+  background-color: ${(props) => updateValue('white', props.backgroundColor)};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: auto;
+  padding: 16px;
+  position: absolute;
+  z-index: 56;
+  border-radius: 25px;
+  left: calc(50% - 166px);
+  top: 25%;
+`;
+
+export const Text = styled.span`
+  font-size: ${(props) => updateFontSize(20, props.fontSize)};
+  font-family: ${(props) => updateValue('EasyReadingPro', props.font)};
+  color: ${colors.primaryTextColor};
+  margin-top: 15px;
+`;
+
+export const Icon = styled.div`
+  img {
+    height: 42px;
+    width: 42px;
+  }
+`;
+
+export const TextSecondary = styled.span`
+  font-size: ${(props) => updateFontSize(16, props.fontSize)};
+  font-family: ${(props) => updateValue('EasyReadingPro', props.font)};
+  color: ${colors.grey};
+  width: 300px;
+  margin-top: 30px;
+`;
+
+export const DarkOverlayContainer = styled.div`
+  position: fixed;
+  z-index: 55;
+  background: rgba(0, 0, 0, 0.8);
+  max-width: 820px;
+  margin: 0px auto;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.5;
+`;
