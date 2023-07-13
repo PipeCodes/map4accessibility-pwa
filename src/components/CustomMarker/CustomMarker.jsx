69183e4befee083a6ed2bacb9437e68f6bc13d99
Pedro Santos <pedro.santos@pipecodes.com>
2023-07-13 18:56:11 +0100

    recenter map after evaluation

--- src/components/CustomMarker/CustomMarker.jsx
+++ src/components/CustomMarker/CustomMarker.jsx
@@ -2,11 +2,12 @@ import React, { useMemo } from 'react';
 import { Marker } from '@react-google-maps/api';
 import { choosePlaceType, markerIcon } from '../../helpers/utils';
 
-const CustomMarker = ({ marker, onClick, clusterer }) => {
+const CustomMarker = ({ marker, onClick, clusterer, isEvaluated }) => {
   const icon = useMemo(
-    () => markerIcon(choosePlaceType(marker?.place_type)),
-    [marker],
+    () => markerIcon(choosePlaceType(marker?.place_type), isEvaluated),
+    [marker, isEvaluated],
   );
+
   const coords = {
     lat: parseFloat(marker.latitude),
     lng: parseFloat(marker.longitude),
