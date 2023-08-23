d1abadd975d9149596e12cb091773d0549dbbc3f
Pedro Santos <pedro.santos@pipecodes.com>
2023-08-23 18:17:36 +0100

    update app after each build

--- src/service-worker.js
+++ src/service-worker.js
@@ -73,8 +73,4 @@ self.addEventListener('message', (event) => {
   }
 });
 
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
 // Any other custom service worker logic can go here.
