6dc1dc02587df3061e101962ac553da2a2b89ee4
Pedro Santos <pedro.santos@pipecodes.com>
2024-05-09 17:13:40 +0100

    ranking screen accessibility

--- src/components/LatestComments/LatestComments.jsx
+++ src/components/LatestComments/LatestComments.jsx
@@ -219,9 +219,10 @@ const LatestComments = (props) => {
                       src={process.env.REACT_APP_EXTERNAL_LINKS_BASE.concat(
                         storageUrl(comment?.app_user?.avatar),
                       ).concat(`/${comment?.app_user?.avatar}`)}
+                      alt="avatar"
                     />
                   ) : (
-                    <Image src={Avatar} />
+                    <Image src={Avatar} alt="avatar" />
                   )}
                   <Name fontSize={fontSize} font={font}>
                     {comment?.app_user &&
@@ -239,6 +240,7 @@ const LatestComments = (props) => {
                         : ''
                     }`}
                     src={getIcon(comment?.evaluation)}
+                    alt={t('alt_text.evaluation_icon')}
                   />
                   <Label fontSize={fontSize} font={font}>
                     {getLabel(comment?.evaluation)}
